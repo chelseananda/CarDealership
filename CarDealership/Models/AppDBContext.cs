1a198f1272d9bf0030fec2010e0f61e6665ad1c2
Chelsea Chelsea <chelseac@shernet.sheridancollege.ca>
2021-12-06 18:40:00 -0500

    Model

---
+++ CarDealership/Models/AppDBContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarDealership.Models
+{
+    //this class is the relation between the web application and the database
+    //Identity dbContext class : in this class we are dealing to manage the identity table in sql server
+    public class AppDBContext : IdentityDbContext
+    {
+        private readonly DbContextOptions _options;
+
+        public AppDBContext(DbContextOptions options) : base(options)
+        {
+            _options = options;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+        }
+    }
+}
--- CarDealership/Startup.cs
+++ CarDealership/Startup.cs
@@ -1,6 +1,9 @@
+using CarDealership.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -11,46 +14,67 @@ using System.Threading.Tasks;
 
 namespace CarDealership
 {
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
+        public class Startup
         {
-            Configuration = configuration;
-        }
 
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddRazorPages();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
+            public Startup(IConfiguration configuration)
             {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                Configuration = configuration;
             }
 
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
+            public IConfiguration Configuration { get; }
+            //reading the connection we put inside the appsettings.json 
+            //initialize our web application to use sql server database
 
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
+            public void ConfigureServices(IServiceCollection services)
             {
-                endpoints.MapRazorPages();
-            });
+                string connectionString = Configuration.GetConnectionString("default");
+                services.AddDbContext<AppDBContext>(c => c.UseSqlServer(connectionString));
+
+                //this will help us to use identtity functionality and handle our database
+                services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<AppDBContext>();
+
+
+                //this part of the code is dealing with client request
+                services.ConfigureApplicationCookie(options =>
+                {
+                    options.LoginPath = "/Account/Login";
+                });
+                //to specify iservices collection which means it will not register the services used for the page
+                services.AddControllersWithViews();
+            }
+
+            // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+            public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+            {
+                if (env.IsDevelopment())
+                {
+                    app.UseDeveloperExceptionPage();
+                }
+                else
+                {
+                    app.UseExceptionHandler("/Home/Error");
+                }
+
+                app.UseStaticFiles();
+
+                app.UseRouting();
+                //enabling the authentication for our web application
+                app.UseAuthentication();
+
+                app.UseAuthorization();
+
+                app.UseEndpoints(endpoints =>
+                {
+                    endpoints.MapControllerRoute(
+                        name: "default",
+                        pattern: "{controller=Home}/{action=Index}/{id?}");
+                });
+            }
+
+            private static IApplicationBuilder GetApp(IApplicationBuilder app)
+            {
+                return app;
+            }
         }
     }
-}
