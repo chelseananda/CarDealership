c7809e213b789e4c6f6e62b904301e75d3d96962
Kevin Tran <tran94@sheridancollege.ca>
2021-12-11 23:33:21 -0500

    created dealer class and moved View models under model. Also added DealerController.

---
+++ CarDealership/Controllers/DealerController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarDealership.Controllers
+{
+    public class DealerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CarDealership/Models/Dealer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarDealership.Models
+{
+    public class Dealer
+    {
+        public float Price { get; set; }
+        public int ProductID { get; set; }
+        public int Quantity { get; set; }
+        public List<Car> Cars { get; set; }
+        public string Name { get; set; }
+        public string location { get; set; }
+
+
+    }
+}
