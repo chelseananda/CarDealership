d07aedaed8dd2679bb8aef15f043595b39915703
chelseanada <chelseac@sheridancollege.ca>
2025-05-03 12:46:11 -0400

    updated

--- CarDealership/Program.cs
+++ CarDealership/Program.cs
@@ -1,11 +1,8 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace CarDealership
 {
@@ -20,7 +17,25 @@ namespace CarDealership
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    // Set up your app startup class (usually Startup.cs)
                     webBuilder.UseStartup<Startup>();
+                })
+                .ConfigureServices((context, services) =>
+                {
+                    // Example: configure application settings with strongly typed options
+                    services.Configure<MyConfigOptions>(context.Configuration.GetSection("MyConfig"));
+                    
+                    // Example: adding logging
+                    services.AddLogging(loggingBuilder =>
+                    {
+                        loggingBuilder.AddConsole();
+                    });
                 });
+    }
+
+    // Example of a configuration class for MyConfig
+    public class MyConfigOptions
+    {
+        public string SomeSetting { get; set; }
     }
 }
